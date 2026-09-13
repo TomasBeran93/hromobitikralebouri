7dbb4e3a07231346ddb1002f18ea257f5b22421b
TomasBeran93 <tomasberan93@gmail.com>
2026-09-13 17:46:45 +0200

    Update quartz.config.ts

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "⚡Hromobití\\nKrále Bouří",
+    pageTitle: "⚡Hromobití"\n"Krále Bouří",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
